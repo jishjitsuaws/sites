fc2471807287c801ce4ab70b39297ba0db55a5c5
pallavic <pallavic@cdac.in>
2025-11-10 12:39:06 +0530

    fs download

--- backend/src/routes/oauth.js
+++ backend/src/routes/oauth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const https = require('https');
+const fs = require('fs');
 
 const ca = fs.readFileSync('/etc/haproxy/certs/sites.pem');
 
